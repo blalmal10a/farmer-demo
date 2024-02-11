b70b2929271ec87e3240827354140d1f50aa90be
ys-grouse <malsawma7777@gmail.com>
2024-02-11 08:18:00 +0530

    initialize project

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,41 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          FARMER DEMO
+        </q-toolbar-title>
+
+
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+<script setup>
+import { ref } from 'vue';
+
+const leftDrawerOpen = ref(false)
+</script>
